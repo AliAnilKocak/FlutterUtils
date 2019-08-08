60cf10289f56b85b5a1fe9092e98a4fda8c5764c
AliAnilKocak <alianilkocak@gmail.com>
2019-08-09 01:38:07 +0300

    Example project created

---
+++ example/lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_utils/flutter_utils.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter_Utils llibrary example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter Util"),
+      ),
+      body: Center(
+        child: Text(FlutterUtils.initial()),
+      ),
+    );
+  }
+}
--- lib/flutter_utils.dart
+++ lib/flutter_utils.dart
@@ -1,7 +1,10 @@
 library flutter_utils;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+class FlutterUtils {
+  static String initial(){
+    return "Flutter Util";
+  }
+
 }
